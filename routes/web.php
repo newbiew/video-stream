3b4aa02dac891a0a0987d00e4948c96713aaf68c
stephen <stephenwong@mafrica.com.my>
2021-06-05 11:07:24 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::group(['middleware' => ['auth']], function(){
+
+//     Route::get('/', [App\Http\Controllers\VideoController::class,'index']);
+
+//     Route::get('/uploader', [App\Http\Controllers\VideoController::class,'uploader'])->name('uploader');
+
+//     Route::post('/upload', [App\Http\Controllers\VideoController::class,'store'])->name('upload');
+// });
+
+// Auth::routes();
+
+
+
+
+Route::get('/',
+    function(){
+        return view('welcome');
+    }
+)->name('/');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
