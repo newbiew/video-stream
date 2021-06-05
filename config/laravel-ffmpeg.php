57de27f0ea2b69af80d064a624faf2bb07598c5e
stephen <stephenwong@mafrica.com.my>
2021-06-05 20:48:16 +0800

    test ffmpeg directory

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,12 +2,12 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', '/usr/bin/ffmpeg'),
         'threads'  => 12,
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', '/usr/bin/ffprobe'),
     ],
 
     'timeout' => 3600,
